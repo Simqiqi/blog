1f7c7c3f70096270968478f55b5c597287dc7484
Simqiqi <simqiqi@outlook.com>
2026-07-16 15:26:56 +0800

    remove supabase multi-user platform: delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// Supabase 配置 - 请填入你的项目信息
-// 注意：GitHub Pages 公开托管，必须使用 anon key，严禁使用 service_role key
-const SUPABASE_URL = 'https://fqypiwfgwmjqcnobclzd.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_cbld-AtKnlXwCkZg7nbTLA_XNDAg2LK';
